3b21a0f71393e7f0cd2858e9762f68e515ed4cab
Tetsuya Ohta <ohta@raw-hide.co.jp>
2013-06-12 01:26:06 +0900

    * Refactoring seed_ext   * Rename module Railstar::ActiveRecordExt to Railstar::SeedExt   * Using ActiveSuport::Concern   * Rewrite test case and test data

--- spec/seed_ext_spec.rb
+++ spec/seed_ext_spec.rb
@@ -1,29 +1,61 @@
 require 'spec_helper'
 
-describe 'SeedExt' do
+describe Railstar::SeedExt do
 
-  context "when truncation!" do
-    before { User.truncation! }
-    it "record's count should == 0" do
-      User.count.should == 0
-    end
+  describe ".truncation!" do
+    before { User.create }
+    subject { User.truncation! }
+    it { expect{ subject }.to change(User, :count).from(1).to(0) }
   end
 
-  describe "truncation" do
+  describe ".truncation" do
     context "when load from csv" do
-      before { User.truncation(:csv, "spec/db/seeds") }
-      it "should seed data" do
-        User.count.should == 2
-        User.find(1).name == '担当者1'
-        User.find(2).sex == 0
+      describe ' check User.count ' do
+        before { User.truncation! }
+        subject { User.truncation(:csv, "spec/db/seeds") }
+        it { expect{ subject }.to change(User, :count).from(0).to(2) }
+      end
+
+      describe ' check loaded data ' do
+        before { User.truncation!
+                 User.truncation(:csv, "spec/db/seeds") 
+               }
+        subject(:users) { 
+          { first: User.first, last: User.last }
+        }
+
+        context ' of first user' do
+          it { expect(users[:first].name).to eq 'admin_from_csv1' }
+          it { expect(users[:first].sex).to eq 1 }
+        end
+
+        context ' of last user' do
+          it { expect(users[:last].name).to eq 'admin_from_csv2' }
+          it { expect(users[:last].sex).to eq 0 }
+        end
       end
     end
+
     context "when load from yml" do
-      before { User.truncation(:yml, "spec/db/seeds") }
-      it "should seed data" do
-        User.count.should == 2
-        User.find(3).name == '担当者3'
-        User.find(4).sex == 1
+      describe ' check User.count ' do
+        before { User.truncation! }
+        subject { User.truncation(:yml, "spec/db/seeds") }
+        it { expect{ subject }.to change(User, :count).by(2).to(2) }
+      end
+
+      describe 'check loaded data' do
+        subject(:users) { 
+          { first: User.first, last: User.last }
+        }
+        context ' of first user' do
+          it { expect(users[:first].name).to eq 'admin_from_yml1' }
+          it { expect(users[:first].sex).to eq 0 }
+        end
+
+        context ' of last user' do
+          it { expect(users[:last].name).to eq 'admin_from_yml2' }
+          it { expect(users[:last].sex).to eq 1 }
+        end
       end
     end
   end
